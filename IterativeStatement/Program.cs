60f51caefb8ba707a0f7985fb301e4d7df1fd8ba
Thomas Yi <thomasyi79@gmail.com>
2020-01-30 17:29:18 -0500

    put in try and catch statements

--- IterativeStatement/Program.cs
+++ IterativeStatement/Program.cs
@@ -10,19 +10,38 @@ namespace IterativeStatement
     {
         static void Main(string[] args)
         {
-            // Display text that tells user to type an integer in between 0 and 100.
-            Console.WriteLine("Please enter an integer between 1 and 100.");
-            // this variable will handle the user input
+            // Ask the user for input
+            Console.Write("Enter an Integer value between 1 and 100 to execute an iterative statement: ");
+            //use try and catch to prevent any value type other than Int.
+            try
+            {
+                //gather data from user
                 string input = Console.ReadLine();
-                // Declaring a variable to count the loop iterations
-                int count = 1;
-                    // WHILE Statement begins
-                    while (count <= 2)
+                // parse string into int.
+                int value_of_input = int.Parse(input);
+                // use if statement to limit input to 1-100
+                if (value_of_input >= 1 && value_of_input <= 100)
+                    // use for loop to perform the iteration and display the messages.
+                    for (int i = 1; i <= value_of_input; i++)
                     {
-                        Console.WriteLine("You have entered" + input + "." + "This is the current integer value in the loop:" + count);
-                    // Add one to count
-                    count = count + 1;
-                    }//end of while statement
+                        Console.WriteLine("You have entered " + value_of_input.ToString() + ". " +
+                            "This is the current integer value in the loop: " + i.ToString());
+                    }
+                //display messages when user enters values outside of limits between 1-100
+                else
+                    Console.WriteLine("Please enter an integer value and try running the program again ...");
+                Console.WriteLine("Press any key to exit the program ...");
+                // Pause the program and await the user to press a key to end the program
+                Console.ReadKey(true);
+            }
+            catch
+            {
+                //display messages when user enters any other data type other than int
+                Console.WriteLine("Please enter an integer value and try running the program again ...");
+                Console.WriteLine("Press any key to exit the program ...");
+                // Pause the program and await the user to press a key to end the program
+                Console.ReadKey(true);
+            }
         }
     }
 }
