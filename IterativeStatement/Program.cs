9fd7aa1e23b72adeb67c600d7258bdd4b99eb1b5
Thomas Yi <thomasyi79@gmail.com>
2020-01-30 00:18:58 -0500

    Add project files.

---
+++ IterativeStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Author: Thomas Yi
+ * Date Created: 1/29/2020
+ * Description: Iterative Statement for Deliverable 2
+ */
+using System;
+
+namespace IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display text that tells user to type an integer in between 0 and 100.
+            Console.WriteLine("Please enter an integer between 1 and 100.");
+            // this variable will handle the user input
+                string input = Console.ReadLine();
+                // Declaring a variable to count the loop iterations
+                int count = 1;
+                    // WHILE Statement begins
+                    while (count <= 2)
+                    {
+                        Console.WriteLine("You have entered" + input + "." + "This is the current integer value in the loop:" + count);
+                    // Add one to count
+                    count = count + 1;
+                    }//end of while statement
+        }
+    }
+}
